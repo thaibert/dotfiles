17e2a64a7978665449d076891ad8fc9d382ef8ce
jeppe-thaibert <jeppe.thaibert@systematic.com>
2022-10-03 14:39:25 +0200

    Handle antigen installation between macOS and debian in a nicer way

--- install.sh
+++ install.sh
@@ -10,3 +10,7 @@ brew bundle install --no-lock
 echo "Stowing dotfiles into home directory ($(echo $HOME))"
 /bin/sh stow.sh
 
+echo "Linking antigen into zsh folder"
+ln --symbolic /usr/share/local/antigen/antigen.zsh $ZSH/antigen.zsh  # macOS
+# ln --symbolic /usr/share/antigen/antigen.zsh $ZSH/antigen.zsh  # debian
+
