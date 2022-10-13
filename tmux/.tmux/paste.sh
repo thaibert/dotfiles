46a84a93cb8b3156656f309a9dadf46895ad199e
jeppe-thaibert <jeppe.thaibert@systematic.com>
2022-10-13 15:59:55 +0200

    Add OS detection for tmux pasting

--- tmux/.tmux/paste.sh
+++ tmux/.tmux/paste.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-# WSL
-"/mnt/c/Program Files/Git/usr/bin/cat.exe" /dev/clipboard
+os="$(uname --kernel-name)"
+case "${os}" in
+	Linux*)		machine="linux";
+			if (grep --quiet --ignore-case "microsoft" /proc/version); then
+				machine="wsl";
+			fi ;;
+	Darwin*)	machine="mac";;
+	*)		machine="UNKNOWN:${os}"
+esac
+
+case "${machine}" in
+	wsl*)	"/mnt/c/Program Files/Git/usr/bin/cat.exe" /dev/clipboard;;
+	linux*)	echo "TODO: linux clipboard integration missing";;
+	mac*)	echo "TODO: macOS clipboard integration missing";;
+	*)	echo "Could not get clipboard: OS unknown: ${machine}"
+esac
 
