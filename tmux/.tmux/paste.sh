02cb6dd74c33de7324559ef193699cc17ff8f4ec
thaibert <github@thaibert.com>
2022-10-19 21:33:58 +0200

    Fix tmux paste script to work on linux AND bsd
    Turns out that `--kernel-name` is not supported on BSD's `uname`, so swapped it with the short-hand `-s` that exists on both.
    
    Also changed tabbing in relation to previous commit. Now it doesn't display wonkily in e.g. `less` which doesn't use my vim-config but instead uses tabs = 8 spaces. Ew.

--- tmux/.tmux/paste.sh
+++ tmux/.tmux/paste.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-os="$(uname --kernel-name)"
+os="$(uname -s)" # -s is --kernel-name
 case "${os}" in
-	Linux*)		machine="linux";
-			if (grep --quiet --ignore-case "microsoft" /proc/version); then
-				machine="wsl";
-			fi ;;
-	Darwin*)	machine="mac";;
-	*)		machine="UNKNOWN:${os}"
+  Linux*)   machine="linux";
+            if (grep --quiet --ignore-case "microsoft" /proc/version); then
+              machine="wsl";
+            fi ;;
+  Darwin*)  machine="mac";;
+  *)        machine="UNKNOWN:${os}";;
 esac
 
 case "${machine}" in
-	wsl*)	"/mnt/c/Program Files/Git/usr/bin/cat.exe" /dev/clipboard;;
-	linux*)	echo "TODO: linux clipboard integration missing";;
-	mac*)	echo "TODO: macOS clipboard integration missing";;
-	*)	echo "Could not get clipboard: OS unknown: ${machine}"
+  wsl*) "/mnt/c/Program Files/Git/usr/bin/cat.exe" /dev/clipboard;;
+  linux*) echo "TODO: linux clipboard integration missing";;
+  mac*) pbpaste;;
+  *) echo "Could not get clipboard: OS unknown: ${machine}"
 esac
 
