fd4a3aad4acd43d46918d49ad8f4b9fc3e85aa32
jeppe-thaibert <jeppe.thaibert@systematic.com>
2022-11-07 13:36:59 +0100

    Fix occasional CRLF-related double newline on WSL
    This dos2unix binary _should_ be distributed with Git Bash, just like the cat binary

--- tmux/.tmux/paste-wsl.sh
+++ tmux/.tmux/paste-wsl.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-"/mnt/c/Program Files/Git/usr/bin/cat.exe" /dev/clipboard
+_cat="/mnt/c/Program Files/Git/usr/bin/cat.exe"
+_dos2unix="/mnt/c/Program Files/Git/usr/bin/dos2unix.exe"
+
+"$_cat" /dev/clipboard | "$_dos2unix"
 
