4c5067a40b54e2a1496bfee0cf6af5fd3864a25b
thaibert <jeppe.thaibert@systematic.com>
2025-03-20 10:36:12 +0100

    scripts: change indentation slightly for ensure-container-running.sh

--- scripts/ensure-container-running.sh
+++ scripts/ensure-container-running.sh
@@ -8,22 +8,22 @@ function test-upness() {
 }
 
 {
-echo "waiting for container to be up..."
+  echo "waiting for container to be up..."
 
-MAX_WAIT_SECS=$((5 * 60))
+  MAX_WAIT_SECS=$((5 * 60))
 
-# Note: `test-upness` _must_ be run in the body of the `until`
-#       because the last executed statement in the body is
-#       the return value of the `until` command.
-timeout=$MAX_WAIT_SECS
-until test-upness "$@" || [ "$timeout" -le 0 ]; do
-  ((--timeout))
-  sleep 1
-  test-upness "$@"
-done
-if [ $? = 0 ]; then
-  exit 0
-else
-  exit 42
-fi
+  # Note: `test-upness` _must_ be run in the body of the `until`
+  #       because the last executed statement in the body is
+  #       the return value of the `until` command.
+  timeout=$MAX_WAIT_SECS
+  until test-upness "$@" || [ "$timeout" -le 0 ]; do
+    ((--timeout))
+    sleep 1
+    test-upness "$@"
+  done
+  if [ $? = 0 ]; then
+    exit 0
+  else
+    exit 42
+  fi
 }
